70fecb83910d7bc52b84df0b2ab279f73fb41bfb
jaykumar286 <jaykumarchavda2806@gmail.com>
2024-04-29 16:55:42 +0530

    add an redis configuration

---
+++ src/config/redisConfig.ts
@@ -0,0 +1,9 @@
+import Redis from 'ioredis';
+import serverConfig from './serverConfig';
+
+const redisConfig = {
+  port: serverConfig.REDIS_PORT as number,
+  host: serverConfig.REDIS_HOST,
+};
+
+export default new Redis(redisConfig);
