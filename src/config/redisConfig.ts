2ee4f8e0ef5ffb46a641b2b17e950f48ebddc136
jaykumar286 <jaykumarchavda2806@gmail.com>
2024-04-29 17:38:42 +0530

    added a sample bullmq producers and workers

--- src/config/redisConfig.ts
+++ src/config/redisConfig.ts
@@ -4,6 +4,7 @@ import serverConfig from './serverConfig';
 const redisConfig = {
   port: serverConfig.REDIS_PORT as number,
   host: serverConfig.REDIS_HOST,
+  maxRetriesPerRequest: null
 };
 
 export default new Redis(redisConfig);
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express, { Express } from "express";
 
 import serverConfig from "./config/serverConfig";
 import apiRouter from "./routes";
+import SampleWorker from "./workers/sampleWoker";
+import sampleProducer from "./producers/sampleProducer";
 
 
 const app: Express = express();
@@ -10,4 +12,13 @@ app.use('/api', apiRouter);
 
 app.listen(serverConfig.PORT, () => {
   console.log(`Server started at *:${serverConfig.PORT}`);
+
+  SampleWorker('SampleQueue');
+
+  sampleProducer('SampleJob', {
+    name: "Tom",
+    age: 20  
+  });
 });
+
+
